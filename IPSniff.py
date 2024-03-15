c74010bfd06d43b139d38f00672fd7b42f9754a3
LYH <a940602718@163.com>
2024-03-15 18:34:18 +0800

    first commit

---
+++ IPSniff.py
@@ -0,0 +1,22 @@
+from scapy.all import *  
+  
+from scapy.all import *
+
+mac_list=[]
+def CallBack(packet):
+    global mac_list
+    if packet.haslayer('ARP'):
+        #如果mac地址不在list[]中，则添加到list[]中，并打印
+        if packet['ARP'].hwsrc not in mac_list:
+            mac_list.append(packet['ARP'].hwsrc)
+            print ("the IP is:"+packet['ARP'].pdst)
+            print ("the MAC is:"+packet['ARP'].hwsrc+"\r\n")
+    time.sleep(1)
+
+filter="arp"
+
+try:
+    sniff(filter=filter, prn=CallBack, iface='Ethernet', timeout=30)
+except KeyboardInterrupt:
+    #捕获Ctrl+C，退出程序
+    print ("exit")
